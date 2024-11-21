3bb687e68632c47a27c237a53e473862c4b540f9
pajuga <pavlej@phoenixnap.com>
2024-11-21 20:07:49 +0000

    fixed issue with mixed node name and machine name

--- controllers/bmcmachine_controller.go
+++ controllers/bmcmachine_controller.go
@@ -599,7 +599,7 @@ func (r *BMCMachineReconciler) BMCClusterToBMCMachines(ctx context.Context) hand
 				result = append(result, ctrl.Request{
 					NamespacedName: client.ObjectKey{
 						Namespace: machine.Namespace,
-						Name:      machine.Spec.InfrastructureRef.Name}})
+						Name:      machine.Name}})
 			}
 		}
 		return result
